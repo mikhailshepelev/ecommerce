325d3902e34c6cd349b8e200fd5059a9f1f90614
mikhailshepelev <romanist-mike@yandex.ru>
2021-02-17 21:02:07 +0200

    section 27 is completed

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa6e80pkq9uyzVEv5d6',
+        issuer: 'https://dev-87338977.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
